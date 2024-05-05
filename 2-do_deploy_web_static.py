f3f1ff0e8ae18755c86f1ecfd4027352112251e3
husham35 <husham35@gmail.com>
2024-05-05 19:14:35 +0000

    task 2 fixes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,32 +33,40 @@ def do_deploy(archive_path):
     """
     if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    try:
+        # Get the archive filename without extension
+        archive_name = os.path.basename(archive_path).split('.')[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        # Upload the archive to /tmp/ directory on the web servers
+        put(archive_path, '/tmp/')
+
+        # Create the release directory
+        release_dir = '/data/web_static/releases/{}'.format(archive_name)
+        run('mkdir -p {}'.format(release_dir))
+
+        # Uncompress the archive to the release directory
+        run('tar -xzf /tmp/{} -C {}'.format(
+            os.path.basename(archive_path), release_dir
+        ))
+
+        # Remove the archive from the web server
+        run('rm /tmp/{}'.format(os.path.basename(archive_path)))
+
+        # Move the files one directory up and delete the folder
+        run('mv {}/web_static/* {}'.format(release_dir, release_dir))
+        run('rm -rf {}/web_static'.format(release_dir))
+
+        # Delete the current symbolic link
+        current_link = '/data/web_static/current'
+        run('rm -rf {}'.format(current_link))
+
+        # Create a new symbolic link
+        run('ln -s {} {}'.format(release_dir, current_link))
+
+        # Restart the nginx service (if necessary)
+        run('sudo service nginx restart')
+
+        return True
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
     
