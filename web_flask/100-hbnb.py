39eee511e5e5147205883c7dfd5a36191fa55291
husham35 <husham35@gmail.com>
2024-05-23 15:38:47 +0000

    advance tasks

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A simple Flask application"""
+from os import getenv
+from flask import Flask, render_template
+
+from models import storage
+
+# Create a Flask web application
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def remove_current_session(exception):
+    """ delete current sqlalchemy session. """
+    storage.close()
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_is_alive():
+    """
+    return and render all states, cities, places and availabe amenities.
+    """
+    states = storage.all('State').values()
+    states = sorted(states, key=lambda state: state.name)
+    amenities = storage.all('Amenity').values()
+    amenities = sorted(amenities, key=lambda amenity: amenity.name)
+    places = storage.all('Place').values()
+    places = sorted(places, key=lambda place: place.name)
+    return render_template(
+        '100-hbnb.html',
+        states=states,
+        amenities=amenities,
+        places=places
+    )
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/7-states_list.sql
+++ web_flask/7-states_list.sql
@@ -17,11 +17,11 @@
 
 -- Drop database
 DROP DATABASE IF EXISTS hbnb_dev_db;
-
+-- hbnb_dev_pwd
 -- Create database + user if doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = '';
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
